2e9fedd2b6096592ffe52380a9ee130bd4eedc2a
forafish <fora.luo@gmail.com>
2018-03-15 19:00:54 +0800

    Fix mutating params in service

--- src/services/chapter/chapter-service.js
+++ src/services/chapter/chapter-service.js
@@ -18,7 +18,7 @@ class ChapterService extends Service {
   }
 
   find (params) {
-    params = params || { query: {} };
+    params = fp.assign({ query: {} }, params);
     params.query.$sort = params.query.$sort || { position: 1 };
 
     return super.find(params);

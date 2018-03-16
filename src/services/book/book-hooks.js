06243880e99f070559185fe0a387f1b408df17ac
forafish <fora.luo@gmail.com>
2018-03-16 11:32:14 +0800

    cache with header 'enrichers-document'

--- src/services/book/book-hooks.js
+++ src/services/book/book-hooks.js
@@ -1,13 +1,17 @@
 import { iff, isProvider } from 'feathers-hooks-common';
 import { associateCurrentUser, queryWithCurrentUser } from 'feathers-authentication-hooks';
 import { hooks } from 'mostly-feathers-mongoose';
+import { cache } from 'mostly-feathers-cache';
 import { hooks as content } from 'playing-content-services';
+
 import BookEntity from '~/entities/book-entity';
 
 module.exports = function(options = {}) {
   return {
     before: {
       all: [
+        hooks.authenticate('jwt', options.auth),
+        cache(options.cache, { headers: ['enrichers-document'] })
       ],
       get: [
         // queryWithCurrentUser({ idField: 'id', as: 'creator' })
@@ -16,13 +20,11 @@ module.exports = function(options = {}) {
         // queryWithCurrentUser({ idField: 'id', as: 'creator' })
       ],
       create: [
-        hooks.authenticate('jwt', options.auth),
         iff(isProvider('external'),
           associateCurrentUser({ idField: 'id', as: 'creator' })),
         content.computePath({ type: 'book' })
       ],
       update: [
-        hooks.authenticate('jwt', options.auth),
         iff(isProvider('external'),
           associateCurrentUser({ idField: 'id', as: 'creator' })),
         hooks.depopulate('parent'),
@@ -31,16 +33,12 @@ module.exports = function(options = {}) {
         content.computeAncestors()
       ],
       patch: [
-        hooks.authenticate('jwt', options.auth),
         iff(isProvider('external'),
           associateCurrentUser({ idField: 'id', as: 'creator' })),
         hooks.depopulate('parent'),
         hooks.discardFields('id', 'metadata', 'ancestors', 'createdAt', 'updatedAt', 'destroyedAt'),
         content.computePath({ type: 'book' }),
         content.computeAncestors()
-      ],
-      remove: [
-        hooks.authenticate('jwt', options.auth)
       ]
     },
     after: {
@@ -48,6 +46,7 @@ module.exports = function(options = {}) {
         hooks.populate('parent', { service: 'folders', fallThrough: ['headers'] }),
         hooks.populate('ancestors'), // with typed id
         hooks.populate('creator', { service: 'users' }),
+        cache(options.cache, { headers: ['enrichers-document'] }),
         hooks.presentEntity(BookEntity, options),
         content.documentEnrichers(options),
         hooks.responder()
--- src/services/chapter/chapter-hooks.js
+++ src/services/chapter/chapter-hooks.js
@@ -1,13 +1,17 @@
 import { iff, isProvider } from 'feathers-hooks-common';
 import { associateCurrentUser, queryWithCurrentUser } from 'feathers-authentication-hooks';
 import { hooks } from 'mostly-feathers-mongoose';
+import { cache } from 'mostly-feathers-cache';
 import { hooks as content } from 'playing-content-services';
+
 import ChapterEntity from '~/entities/chapter-entity';
 
 module.exports = function(options = {}) {
   return {
     before: {
       all: [
+        hooks.authenticate('jwt', options.auth),
+        cache(options.cache, { headers: ['enrichers-document'] })
       ],
       get: [
         // queryWithCurrentUser({ idField: 'id', as: 'creator' })
@@ -23,7 +27,6 @@ module.exports = function(options = {}) {
         content.fetchBlobs({ xpaths: 'files' })
       ],
       update: [
-        hooks.authenticate('jwt', options.auth),
         iff(isProvider('external'),
           associateCurrentUser({ idField: 'id', as: 'creator' })),
         hooks.depopulate('parent'),
@@ -33,7 +36,6 @@ module.exports = function(options = {}) {
         content.fetchBlobs({ xpaths: 'files' })
       ],
       patch: [
-        hooks.authenticate('jwt', options.auth),
         iff(isProvider('external'),
           associateCurrentUser({ idField: 'id', as: 'creator' })),
         hooks.depopulate('parent'),
@@ -41,9 +43,6 @@ module.exports = function(options = {}) {
         content.computePath({ type: 'chapter' }),
         content.computeAncestors(),
         content.fetchBlobs({ xpaths: 'files' })
-      ],
-      remove: [
-        hooks.authenticate('jwt', options.auth)
       ]
     },
     after: {
@@ -51,6 +50,7 @@ module.exports = function(options = {}) {
         hooks.populate('parent', { service: 'books', fallThrough: ['headers'] }),
         hooks.populate('ancestors'), // with typed id
         hooks.populate('creator', { service: 'users' }),
+        cache(options.cache, { headers: ['enrichers-document'] }),
         hooks.presentEntity(ChapterEntity, options),
         content.documentEnrichers(options),
         hooks.responder()

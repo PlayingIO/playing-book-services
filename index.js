fe6c0fcf88a25e6f7ab8eb9ce0b463502d10ca7b
forafish <fora.luo@gmail.com>
2018-04-26 19:12:38 +0800

    remove time log of esm import

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require = require("esm")(module/*, options*/);
-console.time('playing-book-services import');
 module.exports = require('./src/index').default;
 module.exports.DocTypes = require('./src/constants').DocTypes;
 module.exports.entities = require('./src/entities').default;
 module.exports.models = require('./src/models').default;
-console.timeEnd('playing-book-services import');
